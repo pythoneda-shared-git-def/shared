e1c41cd2fc719fa8ef0be7f6f64c31c559bcdeb5
rydnr <github@acm-sl.org>
2023-08-21 10:35:35 +0200

    Updating to pythoneda-shared-git/shared-0.0.1a8

--- shared/flake.nix
+++ shared/flake.nix
@@ -22,13 +22,13 @@
     nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
     pythoneda-shared-pythoneda-banner = {
-      url = "github:pythoneda-shared-pythoneda/banner/0.0.1a12";
+      url = "github:pythoneda-shared-pythoneda/banner/0.0.1a13";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
     };
     pythoneda-shared-pythoneda-domain = {
       url =
-        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a32?dir=domain";
+        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a33?dir=domain";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
@@ -41,8 +41,8 @@
       let
         org = "pythoneda-shared-git";
         repo = "shared";
-        version = "0.0.1a7";
-        sha256 = "sha256-oZwL0EUbk8LabD3tiXHKaL4eUiAXw0aC1bwQ/+xqx1M=";
+        version = "0.0.1a8";
+        sha256 = "sha256-FoQONz/ewSG244FHgPH3gu4+rX/jpce+bGwC6bPGYMk=";
         pname = "${org}-${repo}";
         pythonpackage = "pythoneda.shared.git";
         pkgs = import nixos { inherit system; };
