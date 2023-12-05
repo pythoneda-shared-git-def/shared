42331ac07073e5b7beb967405c792b611779c2ae
rydnr <github@acm-sl.org>
2023-12-05 21:47:39 +0100

    Updated dependencies

--- flake.nix
+++ flake.nix
@@ -24,14 +24,14 @@
     pythoneda-shared-pythoneda-banner = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
-      url = "github:pythoneda-shared-pythoneda-def/banner/0.0.28";
+      url = "github:pythoneda-shared-pythoneda-def/banner/0.0.33";
     };
     pythoneda-shared-pythoneda-domain = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
       inputs.pythoneda-shared-pythoneda-banner.follows =
         "pythoneda-shared-pythoneda-banner";
-      url = "github:pythoneda-shared-pythoneda-def/domain/0.0.5";
+      url = "github:pythoneda-shared-pythoneda-def/domain/0.0.6";
     };
   };
   outputs = inputs:
